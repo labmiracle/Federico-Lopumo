dd4f3feee0d27dcbbb7afb7ab0f82663bdb71061
Federico Lopumo <lopumo65@gmail.com>
2023-04-21 13:36:12 -0300

    ejercicio 3 js

---
+++ Practicas-JS/Ejercicio 3.js
@@ -0,0 +1,17 @@
+function calcularDiasCrecimiento(velocidadCrecimiento, velocidadDecrecimiento, alturaDeseada) {
+    let alturaActual = 0;
+    let dias = 0;
+
+    while (alturaActual < alturaDeseada) {
+        alturaActual += velocidadCrecimiento;
+        dias++;
+        if (alturaActual < alturaDeseada) {
+            alturaActual -= velocidadDecrecimiento;
+        }
+    }
+
+    return dias;
+}
+
+// Ejemplo de uso:
+console.log(calcularDiasCrecimiento(2, 1, 10)); // Devuelve 9
